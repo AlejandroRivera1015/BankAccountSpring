b8b2381d9ad9489fea18b7f371fdb4eb3400a906
AlexDev <ariverar151097@icloud.com>
2024-10-02 22:26:58 -0600

    Update Card Entity

--- src/main/java/com/Bank/Application/Entities/Card.java
+++ src/main/java/com/Bank/Application/Entities/Card.java
@@ -1,7 +1,23 @@
 package com.Bank.Application.Entities;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "cards")
 public class Card {
 
+    @Id
+    private Long id;
     private Long number;
     private Integer passCode;
     private Double balance;
