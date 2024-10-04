1dba7ba0b35deb5c9f38b7ca7a7660f6d4c2f5ef
AlexDev <ariverar151097@icloud.com>
2024-10-04 15:12:55 -0600

    Update User DTO

--- src/main/java/com/Bank/Application/Entities/DTO/UserDTO.java
+++ src/main/java/com/Bank/Application/Entities/DTO/UserDTO.java
@@ -1,24 +1,34 @@
 package com.Bank.Application.Entities.DTO;
 
 import java.sql.Date;
+import java.util.UUID;
 
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import lombok.Setter;
 import lombok.ToString;
 
 @ToString
 @Getter
 @Setter
+@NoArgsConstructor
 public class UserDTO {
 
     private Integer id;
     private String email;
     private String name;
     private String password;
+    private UUID token;
 
     public UserDTO(String email, String password){
         this.email = email;
         this.password = password;
+    }
+
+    public UserDTO(String email, String name, UUID token ){
+        this.email = email;
+        this.token = token;
+        this.name = name;
     }
 
 
